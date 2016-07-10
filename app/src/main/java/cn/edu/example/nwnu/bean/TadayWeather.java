0711d8f7c627fd4aa50085c7f814a6e868548e4c
wuwenchun <496194722@qq.com>
2016-07-10 11:00:43 +0800

    123

---
+++ app/src/main/java/cn/edu/example/nwnu/bean/TadayWeather.java
@@ -0,0 +1,86 @@
+package cn.edu.example.nwnu.bean;
+
+/**
+ * Created by zzp on 2016/7/10.
+ */
+public class TadayWeather {
+    private String city;
+    private String updatetime;
+    private String wendu;
+    private String shidu;
+    private String pm25;
+    private String quality;
+    private String fengxiang;
+    private String fengli;
+    private String date;
+    private String high;
+    private String low;
+    private String type;
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getUpdatetime() {
+        return updatetime;
+    }
+
+    public String getWendu() {
+        return wendu;
+    }
+
+    public String getShidu() {
+        return shidu;
+    }
+
+    public String getPm25() {
+        return pm25;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+
+    public String getFengxiang() {
+        return fengxiang;
+    }
+
+    public String getFengli() {
+        return fengli;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getHigh() {
+        return high;
+    }
+
+    public String getLow() {
+        return low;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "TadayWeather{" +
+                "city='" + city + '\'' +
+                ", updatetime='" + updatetime + '\'' +
+                ", wendu='" + wendu + '\'' +
+                ", shidu='" + shidu + '\'' +
+                ", pm25='" + pm25 + '\'' +
+                ", quality='" + quality + '\'' +
+                ", fengxiang='" + fengxiang + '\'' +
+                ", fengli='" + fengli + '\'' +
+                ", date='" + date + '\'' +
+                ", high='" + high + '\'' +
+                ", low='" + low + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
+
---
+++ app/src/main/java/cn/edu/example/nwnu/myapplication1/SelectCity.java
@@ -0,0 +1,7 @@
+package cn.edu.example.nwnu.myapplication1;
+
+/**
+ * Created by zzp on 2016/7/10.
+ */
+public class SelectCity {
+}
